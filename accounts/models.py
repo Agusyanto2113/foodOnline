a1d945ca037b92b841dc8bf232678a959f821af6
Agusyanto2113 <74976807+Agusyanto2113@users.noreply.github.com>
2024-07-13 18:12:47 +0700

    Complete Vendor

--- accounts/models.py
+++ accounts/models.py
@@ -37,11 +37,11 @@ class UserManager(BaseUserManager):
         return user
 
 class User(AbstractBaseUser):
-    RESTAURANT = 1
+    VENDOR = 1
     CUSTOMER = 2
 
     ROLE_CHOICE = (
-        (RESTAURANT,'Restaurant'),
+        (VENDOR,'Vendor'),
         (CUSTOMER,'Customer'),
     )
 
@@ -76,6 +76,13 @@ class User(AbstractBaseUser):
     def has_module_perms(self,app_label):
         return True
     
+    def get_role(self):
+        if self.role == 1:
+            user_role = 'Vendor'
+        elif self.role == 2:
+            user_role = 'Customer'
+        return user_role
+        
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,blank=True,null=True)
     profile_picture = models.ImageField(upload_to='users/profile_pictures',blank=True,null=True)

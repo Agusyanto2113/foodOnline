cb04d3b4698eb43c92316c00549f81592061df6c
Agusyanto2113 <74976807+Agusyanto2113@users.noreply.github.com>
2024-08-26 22:43:55 +0700

    paypal complete

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpRequest, HttpResponse,JsonResponse
 from django.shortcuts import render,get_object_or_404,redirect
+
+from accounts.models import UserProfile
 from .context_processors import get_cart_amounts, get_cart_counter
 from menu.models import Category,FoodItem
 from django.db.models import Prefetch
@@ -12,6 +14,7 @@ from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D
 from django.contrib.gis.db.models.functions import Distance
 from datetime import date,datetime
+from orders.forms import OrderForm
 # Create your views here.
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True,user__is_active=True)[:8]
@@ -166,3 +169,29 @@ def search(request):
         }
 
         return render(request,'marketplace/listings.html',context)
+
+@login_required(login_url='login')
+def checkout(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('marketplace')
+    
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name':request.user.first_name,
+        'last_name':request.user.last_name,
+        'phone':request.user.phone_number,
+        'email':request.user.email,
+        'address':user_profile.address,
+        'country':user_profile.country,
+        'state':user_profile.state,
+        'city':user_profile.city,
+        'pin_code':user_profile.pin_code,
+    }
+    form = OrderForm(initial=default_values)
+    context = {
+        'form':form,
+        'cart_items':cart_items,
+    }
+    return render(request,'marketplace/checkout.html',context)

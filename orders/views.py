f91944084ebb8ec426101f8ff32827983183cdf0
Agusyanto2113 <74976807+Agusyanto2113@users.noreply.github.com>
2024-08-28 20:04:39 +0700

    razor pay complete

--- orders/views.py
+++ orders/views.py
@@ -8,8 +8,13 @@ import simplejson as json
 from .utils import generate_order_number
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
-
+import razorpay
+from foodOnline_main.settings import RZP_KEY_ID,RZP_KEY_SECRET
 # Create your views here.
+#Razorpay
+client = razorpay.Client(auth=(RZP_KEY_ID, RZP_KEY_SECRET))
+
+
 @login_required(login_url='login')
 def place_order(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
@@ -44,9 +49,26 @@ def place_order(request):
             order.save()
             order.order_number = generate_order_number(order.id)
             order.save()
+
+            DATA = {
+            "amount": float(order.total) * 100,
+            "currency": "INR",
+            "receipt": "receipt#1"+order.order_number,            
+            "notes": {
+                "key1": "value3",
+                "key2": "value2"
+            }
+            }
+            rzp_order = client.order.create(data=DATA)
+            rzp_order_id = rzp_order['id']
+            print(rzp_order)
+
             context = {
                 'order':order,
                 'cart_items':cart_items,
+                'rzp_order_id':rzp_order_id,
+                'RZP_KEY_ID':RZP_KEY_ID,
+                'rzp_amount': float(order.total) * 100,
             }
             return render(request,'orders/place_order.html',context)
         else:

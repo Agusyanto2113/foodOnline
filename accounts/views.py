fc72b7b2666eeb415eeb684723fcd34da6650059
Agusyanto2113 <74976807+Agusyanto2113@users.noreply.github.com>
2024-08-30 21:59:09 +0700

    customer profile complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+
+from orders.models import Order
 from .forms import UserForm
 from .models import User,UserProfile
 from django.contrib import messages,auth
@@ -168,7 +170,14 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def custDashboard(request):
-    return render(request,'accounts/custDashboard.html')
+    orders = Order.objects.filter(user=request.user,is_ordered=True)
+    recent_orders = orders[:5]
+    context = {
+        'orders':orders,
+        'orders_count':orders.count,
+        'recent_orders':recent_orders,
+    }
+    return render(request,'accounts/custDashboard.html',context)
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
